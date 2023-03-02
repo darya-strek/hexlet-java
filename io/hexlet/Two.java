c1f371a362c07912d4fd6f19a84d8629b1569565
Darya Strekalovskaya <darya.strek@gmail.com>
2023-03-02 18:56:35 +0300

    Fix One.java and Two.java

--- io/hexlet/Two.java
+++ io/hexlet/Two.java
@@ -1,7 +1,9 @@
 package io.hexlet;
 
+import org.apache.commons.lang3.StringUtils;
+
 public class Two {
 	public static void greeting() {
-		System.out.println("Java for Brave");
+		System.out.println(StringUtils.reverse("Hexlet"));
 	}
 }
